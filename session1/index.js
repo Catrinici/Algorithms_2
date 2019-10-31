c4b671f9d5c1fc639a19402f4ecc774ab464874e
Catrinici <catrinici.antonina@gmail.com>
2019-10-30 17:57:32 -0700

    3rd commit

--- session1/index.js
+++ session1/index.js
@@ -72,7 +72,63 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 
+function capitalizeLetters(str) {
 
+    //============================//
+    // const strArr = str.toLowerCase().split(' ');
+
+    // for (let i = 0; i < strArr.length; i++) {
+    //     strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+    // }
+
+    // return strArr.join(' ');
+
+    //============================//
+
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(word => word[0].toUpperCase() + word.substr(1))
+        .join(' ');
+
+    //============================//
+
+    // return str.replace(/\b[a-z]/gi, function(char) {
+    //     return char.toUpperCase();
+    // })
+
+}
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+
+function maxCharacter(str) {
+    const charMap = {};
+    let maxNum = 0;
+    let maxChar = '';
+
+    str.split('').forEach(function(char) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+
+        }
+    });
+
+    for (let char in charMap) {
+        if (charMap[char] > maxNum) {
+            maxNum = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
+
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 
 
@@ -83,6 +139,8 @@ function reverseInt(int) {
 // const output = reverseString('hello');
 // const output = isPalindrome('hello');
 // const output = reverseInt(12345);
-const output = reverseInt(-12345);
+// const output = capitalizeLetters('i love javascript');
+// const output = maxCharacter('javascriptttttttt');
+const output = fizzBuzz()
 
 console.log(output);
