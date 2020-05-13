f4e9d1bd6d08882f1d19f791245c0a70a4bc98e5
Catrinici <catrinici.antonina@gmail.com>
2020-05-12 17:13:13 -0700

    algos using recursion

--- HackerHero_algos/recursion.js
+++ HackerHero_algos/recursion.js
@@ -76,3 +76,37 @@ function Zibonacci(num) {
 // Zibonacci(11) to return 11
 
 //5.Given a string, use recursion to return array where each element is a string representing a different anagram (a different sequence of the letters in that string). For example, if given "lim", return ["ilm", "iml", "lim", "lmi", "mil", "mli"].
+
+function stringAnagram(str) {
+    var words = [];
+    _rStringAnagram('', str, words);
+    return words;
+}
+
+function _rStringAnagram(name, rem, words) {
+    var depth = '';
+    for (var i = 0; i < name.length; i++) {
+        depth += "..............";
+    }
+    if (rem == "") {
+        words.push(name);
+        return;
+    }
+    for (var i = 0; i < rem.length; i++) {
+        var new_rem = '';
+        for (var j = 0; j < rem.length; j++) {
+            if (i != j) {
+                new_rem += rem[j];
+            }
+        }
+        console.log(depth + name + rem[i]);
+        _rStringAnagram(name + rem[i], new_rem, words);
+    }
+
+}
+
+StringAnagram("lim").length //to return 6
+
+StringAnagram("lima").length //to return 24
+
+StringAnagram("limas").length //to return 120
